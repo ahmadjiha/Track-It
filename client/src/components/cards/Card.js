7b0387b9f7f563e2bd6dad782ac0ae4c490ef02c
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-06-23 11:35:21 -0700

    Implement Front-End Functionality: User can edit a card's description from the card view.
    Co-authored-by: Ainaa Sakinah <ainaasakinah@gmail.com>

--- client/src/components/cards/Card.js
+++ client/src/components/cards/Card.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { fetchCard } from "../../features/cards/cards";
@@ -9,12 +8,11 @@ import CardDueDate from "./CardDueDate";
 import Labels from "./Labels";
 import SideBar from "./SideBar";
 import DescriptionForm from "./DescriptionForm";
-import { updateCard } from "../../features/cards/cards";
+import CardHeader from "./CardHeader";
 
 const Card = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
-  const [cardTitle, setCardTitle] = useState("");
 
   useEffect(() => {
     dispatch(fetchCard({id}));
@@ -32,18 +30,6 @@ const Card = () => {
 
   const list = lists.find(list => list._id === card.listId);
 
-  const handleSetCardTitle = (event) => {
-    // check: handling state (in-component)
-    event.preventDefault();
-    setCardTitle(event.target.value);
-  }
-
-  const handleUpdateCardTitle = (event) => {
-    event.preventDefault();
-    const cardUpdates = { title: cardTitle }
-    dispatch(updateCard({ id, cardUpdates }));
-  }
-
   return (
     <div id="modal-container">
       <div className="screen"></div>
@@ -51,14 +37,7 @@ const Card = () => {
         <Link to={"/boards/" + card.boardId}>
           <i className="x-icon icon close-modal"></i>
         </Link>
-        <header>
-          <i className="card-icon icon .close-modal"></i>
-          <textarea className="list-title" style={{ height: "45px" }} defaultValue={card.title} onChange={handleSetCardTitle} onBlur={handleUpdateCardTitle}/>
-          <p>
-            in list <a className="link">{list.title}</a>
-            <i className="sub-icon sm-icon"></i>
-          </p>
-        </header>
+        <CardHeader card={card} list={list}/>
         <section className="modal-main">
           <ul className="modal-outer-list">
             <li className="details-section">
--- client/src/components/cards/DescriptionForm.js
+++ client/src/components/cards/DescriptionForm.js
@@ -1,20 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { updateCard } from "../../features/cards/cards";
 
 const DescriptionForm = ({ card }) => {
+  const dispatch = useDispatch();
+  const [description, setDescription] = useState(card.description);
+  const [editFormActive, setEditFormActive] = useState(false);
+
+  const toggleDescriptionEdit = () => {
+    setEditFormActive(!editFormActive);
+  }
+
+  const handleEditClick = event => {
+    event.preventDefault();
+    setEditFormActive(true);
+  }
+
+  const handleXButtonClick = event => {
+    event.preventDefault();
+    setDescription(card.description);
+    toggleDescriptionEdit();
+  }
+
+  const handleEditDescription = event => {
+    event.preventDefault();
+    setDescription(event.target.value);
+  }
+
+  const handleUpdateDescription = event => {
+    event.preventDefault();
+    const id = card._id
+    const cardUpdates = { description }
+    dispatch(updateCard({ id, cardUpdates, callback: toggleDescriptionEdit }));
+  }
+
   return (
     <form className="description">
       <p>Description</p>
-      <span id="description-edit" className="link">
+      <span id="description-edit" className="link" onClick={handleEditClick}>
         Edit
       </span>
-      <p className="textarea-overlay">
-        {card.description}
-      </p>
-      <p id="description-edit-options" className="hidden">
-        You have unsaved edits on this field.{" "}
-        <span className="link">View edits</span> -{" "}
-        <span className="link">Discard</span>
-      </p>
+      {editFormActive ? 
+        <>
+          <textarea className="textarea-toggle" rows="1" autoFocus value={description} onChange={handleEditDescription}/>
+          <div>
+            <div className="button" value="Save" onClick={handleUpdateDescription}>
+              Save
+            </div>
+            <i className="x-icon icon" onClick={handleXButtonClick}></i>
+          </div>
+        </> 
+      :
+        <>
+          <p className="textarea-overlay">
+            {card.description}
+          </p>
+          {/* still need to implement the below */}
+          <p id="description-edit-options" className="hidden">
+            You have unsaved edits on this field.{" "}
+            <span className="link">View edits</span> -{" "}
+            <span className="link">Discard</span>
+          </p>
+        </>
+      }
+
     </form>
   )
 }
